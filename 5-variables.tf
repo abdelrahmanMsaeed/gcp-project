eb3bd0b100426b3574b1a9d6912de2687954bc86
abosaeed7 <abdo.said78788@gmail.com>
2022-01-13 20:55:29 +0200

    first commit

---
+++ 5-variables.tf
@@ -0,0 +1,39 @@
+variable "regoin" {
+    type = string
+}
+
+variable "project_name" {
+    type = string
+}
+
+variable "network_name" {
+    type = string
+}
+
+
+variable "public_subnet_name" {
+    type = string
+}
+
+variable "private_subnet_name" {
+    type = string
+    
+}
+
+variable "router_name" {
+    type = string
+}
+
+variable "nat_cloud" {
+    type = string
+}
+
+variable "vm_type" {
+    type = string
+}
+
+
+
+variable "service_account_vm" {
+    type = string
+}   
